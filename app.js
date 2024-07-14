f2d9cd702dcee420bcf2eebc243a1a00954c4b1c
Len83 <38Len@222>
2024-07-14 18:10:39 +0200

    js updates to 21Twelve webpage

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { engine } = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+// View engine setup
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Static folder
+app.use('/assest', express.static(path.join(__dirname, 'assets')));
+
+let transporter = nodemailer.createTransport({
+  host: "mail.21twelve.africa",
+  port: 465,
+  secure: true, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: "info@21twelve.africa",
+    pass: "Ladysox101!!@",
+  },
+});
+
+app.get('/', (req, res) => {
+  res.render('contact');
+});
+
+app.post('/send', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Company: ${req.body.company}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  async function main() {
+    try {
+      const info = await transporter.sendMail({
+        from: 'Nodemailer Contact form <info@21twelve.africa>', // sender address
+        to: "len@21twelve.africa, radka@21twelve.africa", // list of receivers
+        subject: "Node Contact Request", // Subject line
+        text: "Hello world?", // plain text body
+        html: output, // html body
+      });
+
+      console.log("Message sent: %s", info.messageId);
+      res.render('contact', { msg: 'Email has been sent' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Something went wrong.');
+    }
+  }
+
+  main();
+});
+
+app.listen(3000, () => console.log('Server started on port 3000...'));
